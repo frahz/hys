4eafae752511289958b39dc6965e93032c8adbd1
frahz <me@frahz.dev>
2026-08-24 15:59:38 -0700

    chore: update flake.lock

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    nixpkgs.url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.zst";
   };
 
   outputs =
